f002d90b90529a0b0ac3c46aacbf74119828b82a
Aditya Jangid <116833294+akshay5568@users.noreply.github.com>
2025-11-23 17:51:16 +0530

    Worked on project header still need to improve.

---
+++ Frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+const SearchBar = () => {
+    return (
+        <div>
+            <div>
+                <input type="text" className="p-3 border border-gray-300 rounded-full w-150 text-xl bg-[#ffffff]"/>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
