87b1676265ae3d34e901aea04df7ddbc3966a4c8
Aditya Jangid <116833294+akshay5568@users.noreply.github.com>
2025-12-24 17:32:02 +0530

    SignIn and SignUp page created.

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -7,24 +7,25 @@ import { CgProfile } from "react-icons/cg";
 const Header = () => {
   return (
     <div className="flex w-full items-center justify-between p-3 h-20  border-b border-gray-200">
-      <div className="w-[10%]">
+      <div className="w-[20%] flex items-center gap-10">
         <Link to={"/"}>
           <img src={logo} alt="" />
         </Link>
+        <span>Explore</span>
       </div>
+
       <SearchBar />
       <div className="flex gap-5 items-center">
         <Link to={"/cart"}>
-             <div className="hover:bg-[#6d29d253] rounded-md p-3">
-                  <FaOpencart className="text-xl " />
-             </div>
+          <div className="hover:bg-[#6d29d253] rounded-md p-3">
+            <FaOpencart className="text-xl " />
+          </div>
         </Link>
 
         <Link to={"/profile/87"}>
-          <div className="hover:bg-[#6d29d249] rounded-md p-3" >
-  <CgProfile className="text-xl" />
+          <div className="hover:bg-[#6d29d249] rounded-md p-3">
+            <CgProfile className="text-xl" />
           </div>
-        
         </Link>
       </div>
     </div>
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Provider} from 'react-redux';
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router';
+import appStore from './Store/AppStore.jsx';
 
 createRoot(document.getElementById('root')).render(
+  <Provider store={appStore}>
   <BrowserRouter>
   <StrictMode>
     <App />
   </StrictMode>
   </BrowserRouter>
+  </Provider>
 )
